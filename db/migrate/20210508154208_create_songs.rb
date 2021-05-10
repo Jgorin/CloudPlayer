6260f2def812b7a7938ef903598fda0a9eb07673
Josh Gorin <Josh@Joshuas-MacBook-Pro-2.local>
2021-05-10 10:48:10 -0400

    Added songs model and migration

--- db/migrate/20210508154208_create_songs.rb
+++ db/migrate/20210508154208_create_songs.rb
@@ -2,6 +2,8 @@ class CreateSongs < ActiveRecord::Migration[5.2]
   def change
     create_table :songs do |t|
       t.string :uid, null: false
+      t.string :name, null: false
+      t.string :album_name, null: false
       t.belongs_to :user, null: false
       t.belongs_to :party, null: false
     end
